e7dfc646fe0b7030c1cd7cac8c11932663c37e6a
Hardik <hardikvala24@gmail.com>
2023-01-27 18:46:47 -0800

    Add server. (#3)

--- client/script.js
+++ client/script.js
@@ -21,7 +21,7 @@ function typeText(element, text) {
   let index = 0;
   let interval = setInterval(() => {
     if (index < text.length) {
-      element.innerElement += text.charAt(index);
+      element.innerHTML += text.charAt(index);
       index++;
     } else {
       clearInterval(interval);
@@ -71,6 +71,30 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
 
   loader(messageDiv);
+
+  const response = await fetch('http://localhost:5001', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = 'Something went wrong';
+    alert(err);
+  }
 }
 
 form.addEventListener('submit', handleSubmit);
---
+++ server/server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const configuration = new Configuration({
+    organization: 'org-7bpnZpBOHWzjuNplB6K9oF52',
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello World',
+    })
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+        const response = await openai.createCompletion({
+            model: "text-ada-001",
+            prompt: `Translate this into French:\n\n${prompt}\n\n`,
+            temperature: 0.5,
+            max_tokens: 250,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error });
+    }
+});
+
+app.listen(5001, () => { console.log('Server is running on port http://localhost:5001')});
