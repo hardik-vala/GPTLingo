57ae892b2e918f5221cee8708e70cbd706ad9f5d
Hardik <hardikvala24@gmail.com>
2023-01-28 19:36:52 -0800

    Add client-side timeout for request to server. (#6)

--- client/script.js
+++ client/script.js
@@ -54,6 +54,21 @@ function chatStripe(isAi, value, uniqueId) {
   )
 }
 
+// copied from https://dmitripavlutin.com/timeout-fetch-request/
+async function fetchWithTimeout(resource, options = {}) {
+  // 8 secs
+  const { timeout = 8000 } = options;
+  
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal  
+  });
+  clearTimeout(id);
+  return response;
+}
+
 const handleSubmit = async (e) => {
   e.preventDefault();
   const data = new FormData(form);
@@ -72,28 +87,33 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5001', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      prompt: data.get('prompt')
-    })
-  })
+  try {
+    const response = await fetchWithTimeout('http://localhost:5001', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        prompt: data.get('prompt')
+      })
+    });
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = '';
 
-  clearInterval(loadInterval);
-  messageDiv.innerHTML = '';
+    if (response.ok) {
+      const data = await response.json();
+      const parsedData = data.bot.trim();
 
-  if (response.ok) {
-    const data = await response.json();
-    const parsedData = data.bot.trim();
-
-    typeText(messageDiv, parsedData);
-  } else {
-    const err = await response.text();
+      typeText(messageDiv, parsedData);
+    } else {
+      const err = await response.text();
+      messageDiv.innerHTML = 'Something went wrong';
+      alert(err);
+    }
+  } catch (error) {
+    clearInterval(loadInterval);
     messageDiv.innerHTML = 'Something went wrong';
-    alert(err);
+    alert(error);
   }
 }
 
