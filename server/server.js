b2b00d6f3412e4d7cfb400d644a056c17a76a324
Hardik <hardikvala24@gmail.com>
2023-01-28 19:21:08 -0800

    Server minor improvements (#5)
    * Move OpenAI organization Id to .env.
    
    * Update server health check message.
    
    * Add debug log for server requests.

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const configuration = new Configuration({
-    organization: 'org-7bpnZpBOHWzjuNplB6K9oF52',
+    organization: process.env.OPENAI_API_ORG,
     apiKey: process.env.OPENAI_API_KEY,
 });
 
@@ -18,12 +18,14 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
     res.status(200).send({
-        message: 'Hello World',
+        message: 'Server is running.',
     })
 });
 
 app.post('/', async (req, res) => {
     try {
+        console.log({ req });
+
         const prompt = req.body.prompt;
 
         const response = await openai.createCompletion({
